5f47aff07219a86da1f63b8c0e990e1e301d6c87
Franz Seidel <franz8818@gmail.com>
2022-09-22 03:08:03 +0000

    se creo los models

--- src/main.py
+++ src/main.py
@@ -39,7 +39,24 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/planet', methods=['GET'])
+def handle_hello():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+@app.route('/people', methods=['GET'])
+def handle_hello():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -6,14 +6,74 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
+
+    def __repr__(self):  #OJO CON DOBLE LINEA
+        return '<User %r>' % self.id  #LA INSTAZACION --> ESTAS PIDIENDO UNA LA CLASE QUE ESTAS CREANDO SE CONVIERTA EN UN OBJ
+        #CASTEAR = TRADUCIR EL RESULTADO
+        
+    def serialize(self): #MUCHO OJO LA FUNCIÓN serialize TRANSFORMÁS LA ETIQUETA ILEGIBLE CON QUE TE RESPONDE PYTHON EN UN OBJETO LEGIBLE
+        return {        #FORMULA PARA CREAR ESE PASO (instazacion)
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+            # do not serialize the password, its a security breach
+
+#OJO: AQUÍ HACEMOS LA TABLA DE DE Planets
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='planets', lazy=True)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Planets %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'),
+       nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'),
+       nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id,
+            # do not serialize the password, its a security breach
+        }
+
+
+        #OJO: AQUÍ HACEMOS LA TABLA DE DE People
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='people', lazy=True)
+
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
             # do not serialize the password, its a security breach
         }
